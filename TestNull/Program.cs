3917e1d12b70e4f220c9c917894aadc5a0712c13
mihan1235 <mihan1235@yandex.ru>
2021-04-02 16:22:12 +0300

    Добавьте файлы проекта.

---
+++ TestNull/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestNull
+{
+    class Program
+    {
+        class A {
+            public int a; 
+        
+        }
+        static void Main(string[] args)
+        {
+            A elem1 = null;
+            A elem = null;
+            elem = elem1;
+            elem1 = new A();
+            Console.WriteLine($"elem1: {elem1.a}");
+            Console.WriteLine($"elem: {elem.a}");
+        }
+    }
+}
